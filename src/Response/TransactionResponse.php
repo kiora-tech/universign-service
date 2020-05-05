56666a4b548db770fc1f9012a74fbd4a763363f2
Favareille Thibaud <tfavareille@gmail.com>
2020-05-05 18:12:03 +0200

    Add property class

--- src/Response/TransactionResponse.php
+++ src/Response/TransactionResponse.php
@@ -4,6 +4,13 @@ namespace Globalis\Universign\Response;
 
 use PhpXmlRpc\Value;
 
+/**
+ * Class TransactionResponse
+ * @author Thibaud Favareille <tfavareille@gmail.com>
+ * @package Globalis\Universign\Response
+ * @property string $id
+ * @property string $url
+ */
 class TransactionResponse extends Base
 {
     protected $attributesDefinitions = [
