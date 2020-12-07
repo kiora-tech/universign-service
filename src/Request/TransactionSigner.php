20d9cb15c8251b10880dc16c63b91cad2cd7baa0
Rathgeber Stéphane <stephane@kiora.tech>
2020-12-07 22:13:59 +0100

    Update TransactionSigner.php

--- src/Request/TransactionSigner.php
+++ src/Request/TransactionSigner.php
@@ -3,16 +3,21 @@
 namespace Globalis\Universign\Request;
 
 /**
- * Class TransactionSigner
- * @package Globalis\Universign\Request
- * @method $this setFirstname(string $firstName)
- * @method $this setLastname(string $lastName)
- * @method $this setPhoneNum(string $phoneNum)
+ * @method $this setFirstname(string $firstname)
+ * @method $this setLastname(string $lastname)
+ * @method $this setOrganization(string $organization)
  * @method $this setEmailAddress(string $emailAddress)
+ * @method $this setPhoneNum(string $phoneNum)
+ * @method $this setBirthDate(DateTime $birthDate)
+ * @method $this setUniversignId(string $universignId)
+ * @method $this setProfile(string $profile)
+ * @method $this setRole(string $role)
  * @method $this setSuccessURL(string $successURL)
  * @method $this setCancelURL(string $cancelURL)
  * @method $this setFailURL(string $failURL)
- * @method $this setProfile(string $profile)
+ * @method $this setCertificateType(string $certificateType)
+ * @method $this setIdDocuments(Globalis\Universign\Request\RegistrationRequest $idDocuments)
+ * @method $this setValidationSessionId(string $validationSessionId)
  */
 class TransactionSigner extends Base
 {
